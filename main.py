a01b4813e43ce1235092b210f7e1f3a2ad33359f
Nogdoia <verokrat@gmail.com>
2020-05-23 21:52:38 +0200

    Prvni funkcni verze

---
+++ main.py
@@ -0,0 +1,22 @@
+def encode(key, text):
+    result = ''
+    stored = []
+    i = 0
+    while i < key:
+        line = ''
+        j = i + 0
+        while j < len(text):
+            line += text[j]
+            j += key
+        stored.append(line)
+        i += 1
+    k = 0
+    while k < key:
+        result += stored[k] + '\n'
+        k += 1
+    return result
+
+key = int(input('Key? '))
+text = input('Text? ')
+print(encode(key, text))
+            
